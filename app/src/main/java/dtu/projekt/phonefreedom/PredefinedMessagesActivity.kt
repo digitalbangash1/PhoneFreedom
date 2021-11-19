b29d688bdc73d9f85f492cc9a2a68cdf2df00f4b
digitalbangash1 <digitalbangash1@hotmail.com>
2021-11-19 19:10:59 +0100

    added predefined message activity

--- app/src/main/java/dtu/projekt/phonefreedom/PredefinedMessagesActivity.kt
+++ app/src/main/java/dtu/projekt/phonefreedom/PredefinedMessagesActivity.kt
@@ -6,39 +6,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.TextView
+import android.widget.*
 import dtu.projekt.phonefreedom.databinding.ActivityPredefinedMessagesBinding
 
 class PredefinedMessagesActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityPredefinedMessagesBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPredefinedMessagesBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_predefined_messages)
 
-        var messages = arrayListOf<String>("One", "Two", "Three")
-        binding.listViewPredefinedMessage.isClickable = true
-        binding.listViewPredefinedMessage.adapter = PredefinedMessagesAdapter(this, messages)
-
-
+        var messages = arrayOf<String>("One", "Two", "Three","One", "Two", "Three","One", "Two", "Three")
+        var adapter = PredefinedMessagesAdapter(this, messages)
+        var listViewPredefinedMessage = findViewById<ListView>(R.id.listViewPredefinedMessage)
+        listViewPredefinedMessage.isClickable = true
+        listViewPredefinedMessage.adapter = adapter
+        listViewPredefinedMessage.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, messages[position], Toast.LENGTH_SHORT).show()
+        }
     }
-}
 
 class PredefinedMessagesAdapter (
     private val context: Activity,
-    private val messages: ArrayList<String>)
-    : ArrayAdapter<String>(context, R.layout.activity_predefined_messages_list_item, messages){
+    private val messages: Array<String>)
+    : ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, messages) {
+
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
         var inflater : LayoutInflater = LayoutInflater.from(context)
-        var view : View = inflater.inflate(R.layout.activity_predefined_messages_list_item, null)
+        var view : View = inflater.inflate(R.layout.activity_predefined_messages_list_item, null, false)
         var message : TextView = view.findViewById(R.id.textViewMessage)
         message.setText(messages[position])
-
-        return super.getView(position, convertView, parent)
+        return view
     }
+
+
 }
+
+}
+
