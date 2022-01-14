77cd70269ddb50442f7eef8697de542542b501b1
Strongside-87 <ranfaspIrrac7retwy>
2022-01-14 14:18:55 +0100

    Arrayadapter and View fixed for drop down menu

--- app/src/main/java/dtu/projekt/phonefreedom/SettingsScreen.java
+++ app/src/main/java/dtu/projekt/phonefreedom/SettingsScreen.java
@@ -3,6 +3,8 @@ package dtu.projekt.phonefreedom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class SettingsScreen extends AppCompatActivity {
 
@@ -12,6 +14,14 @@ public class SettingsScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_screen);
+
+        AutoCompleteTextView LangSet = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(SettingsScreen.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Languages));
+        myAdapter.setDropDownViewResource(R.layout.dropdown_item);
+        LangSet.setAdapter(myAdapter);
+
 
     }
 }
