5846925c581248f2308e3a8c5f719e54816d5ecb
salim963 <salimdk97@gmail.com>
2022-01-14 14:05:23 +0100

    time in sms

--- app/src/main/java/dtu/projekt/phonefreedom/MainActivity.kt
+++ app/src/main/java/dtu/projekt/phonefreedom/MainActivity.kt
@@ -137,10 +137,11 @@ class MainActivity : AppCompatActivity() {
         editTextFreeTo.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
 
-            override fun onTextChanged( s: CharSequence, start: Int, before: Int, count: Int) {
-                showtime = s.toString()
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                showtime =  s.toString()
                 var showMyTimeOnAutoText = findViewById<EditText>(R.id.editTextAutoText)
-                showMyTimeOnAutoText.setText( editText.text.toString()+ " jeg tjekker min telefon igen kl:" + showtime)
+                showMyTimeOnAutoText.setText(editText.text.toString() +"jeg tjekker min telefon kl:"+ showtime)
             }
             override fun afterTextChanged(s: Editable) {
             }
