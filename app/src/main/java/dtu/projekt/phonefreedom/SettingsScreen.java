268ec1c82d3da217a890419700ddab0e96da382b
Strongside-87 <ranfaspIrrac7retwy>
2022-01-14 13:38:47 +0100

    Sprog hint og ikon lavet.
    Mangler Arrayadapter

--- app/src/main/java/dtu/projekt/phonefreedom/SettingsScreen.java
+++ app/src/main/java/dtu/projekt/phonefreedom/SettingsScreen.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class SettingsScreen extends AppCompatActivity {
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
